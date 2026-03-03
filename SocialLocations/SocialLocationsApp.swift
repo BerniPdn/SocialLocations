36a33535ec387135e5b3c3e7ad352b0f3adabca3
Bernarda Perez De Nucci <bperezde@macalester.edu>
2026-03-03 16:45:16 -0600

    Initial Commit

---
+++ SocialLocations/SocialLocationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialLocationsApp.swift
+//  SocialLocations
+//
+//  Created by Bernarda Perez De Nucci on 3/3/26.
+//
+
+import SwiftUI
+
+@main
+struct SocialLocationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SocialLocationsTests/SocialLocationsTests.swift
@@ -0,0 +1,17 @@
+//
+//  SocialLocationsTests.swift
+//  SocialLocationsTests
+//
+//  Created by Bernarda Perez De Nucci on 3/3/26.
+//
+
+import Testing
+@testable import SocialLocations
+
+struct SocialLocationsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
