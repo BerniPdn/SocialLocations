f472ed66fbc501b4e22fa532337897a3eb6817a1
IreneGallini <igallini@macalester.edu>
2026-04-03 11:19:28 -0500

    user models

---
+++ SocialLocations/ProfileTab/AppUser.swift
@@ -0,0 +1,19 @@
+//
+//  AppUser.swift
+//  SocialLocations
+//
+//  Created by Irene Gallini on 4/3/26.
+//
+
+import Foundation
+import FirebaseFirestore
+
+struct AppUser: Identifiable, Codable {
+    @DocumentID var id: String?
+    let username: String
+    let usernameLower: String
+    let phoneNumber: String
+    let profileImageURL: String
+    let email: String
+    let friendIDs: [String]
+}
