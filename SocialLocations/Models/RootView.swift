badc36f2b317b984627f6ef7c48ef95140c3ba69
Bernarda Perez De Nucci <bperezde@macalester.edu>
2026-04-09 13:17:41 -0500

    commented rootview out

--- SocialLocations/Models/RootView.swift
+++ SocialLocations/Models/RootView.swift
@@ -4,19 +4,19 @@
 //
 //  Created by Irene Gallini on 4/3/26.
 //
-
-import SwiftUI
-
-struct RootView: View {
-    @EnvironmentObject var authViewModel: AuthViewModel
-
-    var body: some View {
-        Group {
-            if authViewModel.user != nil {
-                MainView()
-            } else {
-                AppUser()
-            }
-        }
-    }
-}
+//
+//import SwiftUI
+//
+//struct RootView: View {
+//    @EnvironmentObject var authViewModel: AuthViewModel
+//
+//    var body: some View {
+//        Group {
+//            if authViewModel.user != nil {
+//                MainView()
+//            } else {
+//                AppUser()
+//            }
+//        }
+//    }
+//}
