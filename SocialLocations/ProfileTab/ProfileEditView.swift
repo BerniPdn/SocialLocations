b3e23db3279defe3498cc0004f6480d79bdab3f7
Bernarda Perez De Nucci <bperezde@macalester.edu>
2026-04-28 16:38:13 -0500

    Update ProfileEditView.swift

--- SocialLocations/ProfileTab/ProfileEditView.swift
+++ SocialLocations/ProfileTab/ProfileEditView.swift
@@ -19,51 +19,56 @@ struct ProfileEditView: View {
     
     var body: some View {
         NavigationStack{
-            VStack(spacing: 20) {
-                PhotosPicker(selection: $selectedItem, matching: .images) {
-                    if let selectedPhoto = selectedImage {
-                        selectedPhoto
-                            .resizable()
-                            .scaledToFill()
-                            .frame(width: 120, height: 120)
-                            .clipShape(Circle())
-                    } else {
-                        Image(systemName: "person.crop.circle.fill")
-                            .resizable()
-                            .frame(width: 120, height: 120)
-                            .foregroundColor(.gray)
-                    }
-                    
-                }
-                TextField("Username", text: $username)
-                    .textFieldStyle(.roundedBorder)
-                    .padding(.horizontal)
-                TextField("Phone Number", text: $phoneNumber)
-                    .textFieldStyle(.roundedBorder)
-                    .padding(.horizontal)
-                Button {
-                    authViewModel.updateProfile(username: username, phoneNumber: phoneNumber) {success in
-                        if success {
-                            dismiss()
-                        }}
-                    
-                } label: {
-                    Text("Confirm Profile Changes")
-                    
-                }
-            }
-            .navigationTitle("Edit Profile")
-            .onAppear {
-                username = authViewModel.appUser?.username ?? ""
-                phoneNumber = authViewModel.appUser?.phoneNumber ?? ""
-            }
-            .onChange(of: selectedItem) {_, newItem in
-                Task {
-                    if let data = try? await newItem?.loadTransferable(type: Image.self) {
-                        selectedImage = data
-                    }
-                }
+            ZStack{
+                AppBackground()
                 
+                VStack(spacing: 20) {
+                    PhotosPicker(selection: $selectedItem, matching: .images) {
+                        if let selectedPhoto = selectedImage {
+                            selectedPhoto
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 120, height: 120)
+                                .clipShape(Circle())
+                        } else {
+                            Image(systemName: "person.crop.circle.fill")
+                                .resizable()
+                                .frame(width: 120, height: 120)
+                                .foregroundColor(.gray)
+                        }
+                        
+                    }
+                    TextField("Username", text: $username)
+                        .textFieldStyle(.roundedBorder)
+                        .padding(.horizontal)
+                    TextField("Phone Number", text: $phoneNumber)
+                        .textFieldStyle(.roundedBorder)
+                        .padding(.horizontal)
+                    Button {
+                        authViewModel.updateProfile(username: username, phoneNumber: phoneNumber) {success in
+                            if success {
+                                dismiss()
+                            }}
+                        
+                    } label: {
+                        Text("Confirm Profile Changes")
+                        
+                    }
+                    .buttonStyle(PrimaryButtonStyle())
+                }
+                .navigationTitle("Edit Profile")
+                .onAppear {
+                    username = authViewModel.appUser?.username ?? ""
+                    phoneNumber = authViewModel.appUser?.phoneNumber ?? ""
+                }
+                .onChange(of: selectedItem) {_, newItem in
+                    Task {
+                        if let data = try? await newItem?.loadTransferable(type: Image.self) {
+                            selectedImage = data
+                        }
+                    }
+                    
+                }
             }
         }
     }
